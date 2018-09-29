2eb63ccb8972b32614f89ec4a67e48c6382303b7
Andrew Elkouri <andrewelkouri@gmail.com>
2018-09-29 18:01:09 -0400

    added reps and weight inputs

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Select from 'react-select';
 
-/*
-  Calculator
-  Exercise
-  Reps
-  Weight
-
-  ...
-  Units
-
-*/
-
 const exerciseOptions = [
   { value: 'squat', label: 'Squat' },
   { value: 'bench', label: 'Bench Press' },
@@ -20,23 +9,57 @@ const exerciseOptions = [
 ];
 
 class Exercise extends React.Component {
-  state = {
-    selectedExercise: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedExercise: '',
+      numberReps: 0,
+      weight: 0,
+    };
+
+    this.handleRepChange = this.handleRepChange.bind(this);
+    this.handleWeightChange = this.handleWeightChange.bind(this);
   }
 
-  handleChange = (selectedExercise) => {
+  handleExerciseChange = (selectedExercise) => {
     this.setState({ selectedExercise });
+  }
+
+  handleRepChange(evt) {
+    if (getNumber(evt.target.value)) {
+      this.setState({ numberReps: evt.target.value });
+    }
+  }
+
+  handleWeightChange(evt) {
+    if (getNumber(evt.target.value)) {
+      this.setState({ weight: evt.target.value});
+    }
   }
 
   render() {
     const { selectedExercise } = this.state;
 
     return (
-      <Select
-        value={selectedExercise}
-        onChange={this.handleChange}
-        options={exerciseOptions}
-      />
+      <div>
+        <Select
+          value={selectedExercise}
+          onChange={this.handleExerciseChange}
+          options={exerciseOptions}
+        />
+
+        <input
+          label="Reps"
+          value={this.state.numberReps}
+          onChange={this.handleRepChange}
+        />
+
+        <input
+          label="Weight"
+          value={this.state.weight}
+          onChange={this.handleWeightChange}
+        />
+      </div>
     );
   }
 }
@@ -54,3 +77,8 @@ ReactDOM.render(
   <Calculator />,
   document.getElementById('root')
 );
+
+function getNumber(data) {
+  const re = /^[0-9\b]+$/;
+  return data === '' || re.test(data);
+}
